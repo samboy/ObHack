8228a8efbabfabc0cda27a556b203202640fd31f
Sam Trenholme <strenholme.usenet@gmail.com>
2015-06-22 19:22:34 -0700

    Another copyright update

--- ObHack-engine-008.1/gui/g_cookie.cc
+++ ObHack-engine-008.1/gui/g_cookie.cc
@@ -3,6 +3,7 @@
 //------------------------------------------------------------------------
 //
 //  Oblige Level Maker (C) 2006,2007 Andrew Apted
+//  Changes (C) 2007-2015 Sam Trenholme and Fritz
 //
 //  This program is free software; you can redistribute it and/or
 //  modify it under the terms of the GNU General Public License
