eeef76763a55f3f7a2ad0accfa204ca15bb01735
Sam Trenholme <strenholme.usenet@gmail.com>
2020-02-10 12:48:41 -0800

    This is finding a needle in a haystack I am adding some code which alters the Lua code so that we can log each and every call to the RNG.  Then, when it varies, we can see in which code the variance happened.
    See https://github.com/samboy/ObHack/issues/8 the code still varies
    even when given the same seed (a lot less than before, but still).

---
+++ ObHack-engine-697/misc/debug_stuff/line_numbers_rand_calls.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+
+# This is a script which will, for all rand_* calls, add a filename and line
+# number so we can debug cases where the code is non-deterministic
+
+# Donated to the public domain by Sam Trenholme in 2020
+
+FILE="$a"
+
+if [ -z "$FILE" ] ; then
+	echo Usage: $0 {filename}
+	exit 1
+fi
+
+# rand_odds
+cat $FILE | perl -pe '
+	$line++;s/(rand_odds\([^\)]*)/\1,\"'$FILE' line $line\"/' > foo
+mv foo $FILE
+
+# rand_range
+cat $FILE | perl -pe '
+	$line++;s/(rand_range\([^\)]*)/\1,\"'$FILE' line $line\"/' > foo
+mv foo $FILE
+
+# rand_irange
+cat $FILE | perl -pe '
+	$line++;s/(rand_irange\([^\)]*)/\1,\"'$FILE' line $line\"/' > foo
+mv foo $FILE
+
+# rand_sel
+cat $FILE | perl -pe '
+	$line++;s/(rand_sel\([^\)]*)/\1,\"'$FILE' line $line\"/' > foo
+mv foo $FILE
+
+# rand_index_by_probs
+cat $FILE | perl -pe '
+	$line++;s/(rand_index_by_probs\([^\)]*)/\1,\"'$FILE' line $line\"/' > foo
+mv foo $FILE
+
+# rand_skew (which does not take any arguments, so no comma here)
+cat $FILE | perl -pe '
+	$line++;s/(rand_skew\([^\)]*)/\1\"builder.lua line $line\"/' > foo
+mv foo $FILE
