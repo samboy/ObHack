70e2344b0b1feb9023ab389cc8bb5d2f06589219
Sam Trenholme <strenholme.usenet@gmail.com>
2020-02-12 07:51:38 -0800

    Add shell script to compile in CentOS 7

---
+++ compile.CentOS7.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+
+export FLAGS=-O3
+export CFLAGS=-O3
+cd fltk-1.1.9
+./configure
+make
+cd ..
+cd zlib-1.2.3
+./configure
+make
+cd ..
+cd glbsp-2.24-source
+cp Makefile.unx Makefile
+make
+cd ..
+cd ObHack-engine-697
+cd gui
+cp Makefile.CentOS7 Makefile
+make
