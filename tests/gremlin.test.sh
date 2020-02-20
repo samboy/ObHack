3721a6da787e861f1e18a76a0c63599b432ab1a2
Sam Trenholme <strenholme.usenet@gmail.com>
2020-02-20 06:44:49 -0800

    Adding test to generate 100 megawads. I found a bug where sometimes ObHack crashes trying to make a megawad

---
+++ tests/gremlin.test.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+
+# This is a "gremlin" test: We build 100 megawads to ensure that there are
+# no fatal errors which occur when attempting to make a wad
+
+if [ ! -e $HOME/tmp ] ; then
+    echo Please mkdir $HOME/tmp
+    exit 1
+fi
+if [ ! -d ../ObHack-engine-697 ] ; then
+    echo Please run this from the tests directory
+    exit 1
+fi
+
+cp ../ObHack-engine-697/scripts/builder.lua $HOME/tmp/builder-save.lua
+
+for A in 0 1 2 3 4 5 6 7 8 9 ; do 
+    for B in 0 1 2 3 4 5 6 7 8 9 0 ; do 
+        ./deterministic.test.sh ${A}FreeDoom${B} | \
+          tail -35 > $HOME/tmp/${A}FreeDoom${B}.out
+    done
+done
